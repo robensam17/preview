1384714c893c7170053cf6b205fdb5593b3892cb
robensam17 <robbu@live.nl>
2020-09-02 11:45:46 +0200

    Add files via upload
    Final touch

---
+++ ani.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#12").click(function() {
+    $('.tex').toggleClass('transform');
+});
+});
+
+
+$(document).ready(function() {
+  $("#13").click(function() {
+    $('.container').toggleClass('transform');
+});
+});
+
+$(document).ready(function() {
+  $("#14").click(function() {
+    $('.tex').toggleClass('transform');
+});
+});
+
+
+
+$(document).ready(function() {
+  $("#15").click(function() {
+    $('.container').toggleClass('transform');
+});
+});
