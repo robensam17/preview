177e4741167121076da622788df9fa8910e955a5
robensam17 <robbu@live.nl>
2020-09-03 13:47:06 +0200

    Add files via upload
    new Style

--- ani.js
+++ ani.js
@@ -1,26 +1,82 @@
 $(document).ready(function() {
+  $("#10").click(function() {
+    $('.frame-5').addClass('transform-active');
+    $('.container').removeClass('transform-active');
+    $('.tex').removeClass('transform-active');
+});
+});
+
+$(document).ready(function() {
+  $("#11").click(function() {
+    $('.frame-5').addClass('transform-active');
+    $('.container').removeClass('transform-active');
+    $('.tex').removeClass('transform-active');
+});
+});
+
+$(document).ready(function() {
   $("#12").click(function() {
-    $('.tex').toggleClass('transform');
+    $('.tex').addClass('transform-active');
+    $('.container').removeClass('transform-active');
+    $('.frame-5').removeClass('transform-active');
 });
 });
 
 
 $(document).ready(function() {
   $("#13").click(function() {
-    $('.container').toggleClass('transform');
+    $('.container').addClass('transform-active');
+      $('.tex').removeClass('transform-active');
+        $('.frame-5').removeClass('transform-active');
 });
 });
 
 $(document).ready(function() {
   $("#14").click(function() {
-    $('.tex').toggleClass('transform');
+    $('.tex').addClass('transform-active');
+    $('.container').removeClass('transform-active');
+      $('.frame-5').removeClass('transform-active');
 });
 });
-
 
 
 $(document).ready(function() {
   $("#15").click(function() {
-    $('.container').toggleClass('transform');
+    $('.container').addClass('transform-active');
+      $('.tex').removeClass('transform-active');
+        $('.frame-5').removeClass('transform-active');
 });
 });
+
+
+
+var attempt = 3;
+  function validate(){
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+    if ( username == "Admin" && password == "Admin"){
+      window.location = "target.html";
+    }
+    else{
+    attempt --;
+
+    if( attempt == 0){
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("submit").disabled = true;
+    return false;
+    }
+    }
+};
+
+
+
+
+
+
+// $(document).ready(function() {
+// let txtMsg = document.getElementById("con1");
+// localStorage.something = txtMsg ;
+
+// document.getElementById('newcon').innerHTML = localStorage.something  ;
+// });
